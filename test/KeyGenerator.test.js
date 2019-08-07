bf11a0d3e6965397fa8ed251d8d4ce4be250460a
ggs134 <inditow@gmail.com>
2019-08-08 05:26:24 +0900

    publicKey verify(), importSk() added

--- test/KeyGenerator.test.js
+++ test/KeyGenerator.test.js
@@ -10,8 +10,17 @@ describe('# PublicKey Test', () => {
   it('#fromPrivate() Test', () => {
     let sk = '1997011358982923168928344992199991480689546837621580239342656433234255379025';
     let privKey = new PrivateKey(sk);
-    let dummyPubKeyObj = new PublicKey();
-    let pubKey = dummyPubKeyObj.fromPrivate(privKey);
+    let pubKey = PublicKey.fromPrivate(privKey);
+    let expectedX = "14897476871502190904409029696666322856887678969656209656241038339251270171395";
+    let expectedY = "16668832459046858928951622951481252834155254151733002984053501254009901876174";
+    pubKey.p.x.n.should.be.bignumber.equal(expectedX);
+    pubKey.p.y.n.should.be.bignumber.equal(expectedY);
+  });
+  it('#importPrivate() Test', () => {
+    let sk = '1997011358982923168928344992199991480689546837621580239342656433234255379025';
+    let privKey = new PrivateKey(sk);
+    let dummyPublicKey = new PublicKey();
+    let pubKey = dummyPublicKey.importPrivate(privKey);
     let expectedX = "14897476871502190904409029696666322856887678969656209656241038339251270171395";
     let expectedY = "16668832459046858928951622951481252834155254151733002984053501254009901876174";
     pubKey.p.x.n.should.be.bignumber.equal(expectedX);
@@ -20,12 +29,18 @@ describe('# PublicKey Test', () => {
   it('#pubkey(random) works?', () => {
     let sk = PrivateKey.getRandObj().field;
     let privKey = new PrivateKey(sk);
-    let dummyPubKeyObj = new PublicKey();
-    let pubKey = dummyPubKeyObj.fromPrivate(privKey);
+    let pubKey = PublicKey.fromPrivate(privKey);
     let expectedX = pubKey.p.x.n;
     let expectedY = pubKey.p.y.n;
     pubKey.p.x.n.should.be.bignumber.equal(expectedX);
     pubKey.p.y.n.should.be.bignumber.equal(expectedY);
+  });
+  it('#pubkey.verify(sk) works?', () => {
+    let sk = PrivateKey.getRandObj().field;
+    let privKey = new PrivateKey(sk);
+    let pubKey = PublicKey.fromPrivate(privKey);
+    let result = pubKey.verifySk(privKey);
+    result.should.be.true;
   });
 });
 
